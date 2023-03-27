841038c8d48d859ef211ab4c7ffaded46989c770
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-03-27 10:03:48 +0700

    Multiple query params & multiple params values

--- query_param_test.go
+++ query_param_test.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -24,6 +25,42 @@ func TestSayHello(t *testing.T) {
 	SayHello(recorder, request)
 
 	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
+}
+
+func MultipleQueryParam(writer http.ResponseWriter, request *http.Request) {
+	firstName := request.URL.Query().Get("first_name")
+	lastName := request.URL.Query().Get("last_name")
+
+	fmt.Fprintf(writer, "Hello %s %s ", firstName, lastName)
+}
+
+func TestMultipleQueryParam(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/hello?first_name=taufiq&last_name=kurniawan", nil)
+	recorder := httptest.NewRecorder()
+
+	MultipleQueryParam(recorder, request)
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
+}
+
+func MultipleParamValues(writer http.ResponseWriter, request *http.Request) {
+	query := request.URL.Query()
+	names := query["name"]
+	fmt.Fprint(writer, strings.Join(names, " "))
+}
+
+func TestMultipleParamValue(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/hello?name=taufiq&name=kurniawan", nil)
+	record := httptest.NewRecorder()
+
+	MultipleParamValues(record, request)
+
+	response := record.Result()
 	body, _ := io.ReadAll(response.Body)
 
 	fmt.Println(string(body))
