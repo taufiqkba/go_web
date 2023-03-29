14004557dd9972cfa3087f3ed6f832fcc8853a52
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-03-29 15:37:03 +0700

    Template action using if else

---
+++ template_action_test.go
@@ -0,0 +1,27 @@
+package go_web
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TemplateActionIf(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles("./templates/if.gohtml"))
+	t.ExecuteTemplate(w, "if.gohtml", Page{
+		Title: "Template Action",
+		Name:  "TKBA",
+	})
+}
+
+func TestTemplateActionIf(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateActionIf(recorder, request)
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
