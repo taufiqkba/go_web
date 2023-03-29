40ed0e57bb51512ed50e290de948484110d4b1d6
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-03-30 01:21:37 +0700

    Template layout html

---
+++ template_layout_test.go
@@ -0,0 +1,32 @@
+package go_web
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TemplateLayout(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles(
+		"./templates/header.gohtml",
+		"./templates/footer.gohtml",
+		"./templates/layout.gohtml",
+	))
+	t.ExecuteTemplate(w, "layout", map[string]interface{}{
+		"Title": "Address Action",
+		"Name":  "Taufiqkba",
+	})
+}
+
+func TestTemplateLayout(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateLayout(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
