6a6e6fcb19dd221fa55bc837c6cd73e609e7c945
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-03-29 16:20:17 +0700

    Using With Action for html template

--- template_action_test.go
+++ template_action_test.go
@@ -9,6 +9,7 @@ import (
 	"testing"
 )
 
+// Using If Else
 func TemplateActionIf(w http.ResponseWriter, r *http.Request) {
 	t := template.Must(template.ParseFiles("./templates/if.gohtml"))
 	t.ExecuteTemplate(w, "if.gohtml", Page{
@@ -26,6 +27,7 @@ func TestTemplateActionIf(t *testing.T) {
 	fmt.Println(string(body))
 }
 
+// Comparison Operator
 func TemplateOperator(w http.ResponseWriter, r *http.Request) {
 	t := template.Must(template.ParseFiles("./templates/comparator.gohtml"))
 	t.ExecuteTemplate(w, "comparator.gohtml", map[string]interface{}{
@@ -43,6 +45,7 @@ func TestTemplateActionOperator(t *testing.T) {
 	fmt.Println(string(body))
 }
 
+// Using Range
 func TemplateActionRange(w http.ResponseWriter, r *http.Request) {
 	t := template.Must(template.ParseFiles("./templates/range.gohtml"))
 	t.ExecuteTemplate(w, "range.gohtml", map[string]interface{}{
@@ -58,6 +61,28 @@ func TestTemplateActionRange(t *testing.T) {
 	recorder := httptest.NewRecorder()
 
 	TemplateActionRange(recorder, request)
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+// Using With
+func TemplateActionWith(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles("./templates/address.gohtml"))
+	t.ExecuteTemplate(w, "address.gohtml", map[string]interface{}{
+		"Title": "Address Action",
+		"Name":  "Taufiqkba",
+		"Address": map[string]interface{}{
+			"Street": "Do not have street name",
+			"City":   "Jakarta",
+		},
+	})
+}
+
+func TestTemplateActionWith(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateActionWith(recorder, request)
 	body, _ := io.ReadAll(recorder.Result().Body)
 	fmt.Println(string(body))
 }
