3f7b5fb266d446f6868c425cfc30f6e074a99115
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-03-29 10:05:47 +0700

    Server file server manual using http and using go embed

---
+++ serve_file_test.go
@@ -0,0 +1,56 @@
+package go_web
+
+import (
+	_ "embed"
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func ServeFile(writer http.ResponseWriter, request *http.Request) {
+	if request.URL.Query().Get("name") != "" {
+		http.ServeFile(writer, request, "./resources/ok.html")
+	} else {
+		http.ServeFile(writer, request, "./resources/notfound.html")
+	}
+}
+
+func TestServeFileServer(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(ServeFile),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+// serve file server using go embed
+
+//go:embed resources/ok.html
+var resourceOk string
+
+//go:embed resources/notfound.html
+var resourceNotFound string
+
+func ServeFileEmbed(writer http.ResponseWriter, request *http.Request) {
+	if request.URL.Query().Get("name") != "" {
+		fmt.Fprint(writer, resourceOk)
+	} else {
+		fmt.Fprint(writer, resourceNotFound)
+	}
+}
+
+func TestServeFileEmbed(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(ServeFileEmbed),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
