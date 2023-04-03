233dd2489d0220f1b94cdcbae21be0a295be7496
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-04-03 10:52:46 +0700

    Error handler using middleware

--- middleware_test.go
+++ middleware_test.go
@@ -17,6 +17,22 @@ func (middleware *LogMiddleware) ServeHTTP(writer http.ResponseWriter, request *
 	fmt.Println("After Execute Handler")
 }
 
+type ErrorHandler struct {
+	Handler http.Handler
+}
+
+func (errorHandler ErrorHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	defer func() {
+		err := recover()
+		if err != nil {
+			fmt.Println("Error")
+			writer.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(writer, "Error: %s", err)
+		}
+	}()
+	errorHandler.Handler.ServeHTTP(writer, request)
+}
+
 func TestMiddleware(t *testing.T) {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
@@ -27,14 +43,22 @@ func TestMiddleware(t *testing.T) {
 		fmt.Println("Foo Execute")
 		fmt.Fprintf(writer, "Foo Middleware")
 	})
+	mux.HandleFunc("/panic", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Println("Panic Execute")
+		panic("Ups")
+	})
 
 	logMiddleware := &LogMiddleware{
 		Handler: mux,
 	}
 
+	errorHandler := &ErrorHandler{
+		Handler: logMiddleware,
+	}
+
 	server := http.Server{
 		Addr:    "localhost:8080",
-		Handler: logMiddleware,
+		Handler: errorHandler,
 	}
 
 	err := server.ListenAndServe()
